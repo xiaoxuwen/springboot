fd76e83b048ad4dd8a4614b07af7dbcb48edf307
= <=>
2017-09-13 13:30:20 +0800

    第二篇：Spring Boot配置文件详解

---
+++ springboot-config/src/main/java/com/hust/springboot/SpringbootConfigApplication.java
@@ -0,0 +1,13 @@
+package com.hust.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfigApplication.class, args);
+	}
+
+}
---
+++ springboot-config/src/main/java/com/hust/springboot/web/LucyController.java
@@ -0,0 +1,32 @@
+package com.hust.springboot.web;
+
+import com.hust.springboot.bean.ConfigBean;
+import com.hust.springboot.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fangzhipeng on 2017/4/18.
+ */
+@RestController
+@EnableConfigurationProperties({ConfigBean.class, User.class})
+public class LucyController {
+    @Autowired
+    ConfigBean configBean;
+
+    @RequestMapping(value = "/lucy")
+    public String miya() {
+        return configBean.getGreeting() + " >>>>" + configBean.getName() + " >>>>" + configBean.getUuid() + " >>>>" + configBean.getMax();
+    }
+
+    @Autowired
+    User user;
+
+    @RequestMapping(value = "/user")
+    public String user() {
+        return user.getName() + user.getAge();
+    }
+
+}
