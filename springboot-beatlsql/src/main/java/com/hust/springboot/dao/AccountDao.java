649a3f8fd5cc0e14dc15b1d5fec3a120d091af33
= <=>
2017-09-13 15:59:16 +0800

    springboot整合 beatlsql

---
+++ springboot-beatlsql/src/main/java/com/hust/springboot/dao/AccountDao.java
@@ -0,0 +1,15 @@
+package com.hust.springboot.dao;
+
+
+import com.hust.springboot.entity.Account;
+import org.beetl.sql.core.annotatoin.SqlStatement;
+import org.beetl.sql.core.mapper.BaseMapper;
+
+/**
+ * Created by fangzhipeng on 2017/4/20.
+ */
+public interface AccountDao extends BaseMapper<Account> {
+
+    @SqlStatement(params = "name")
+    Account selectAccountByName(String name);
+}
