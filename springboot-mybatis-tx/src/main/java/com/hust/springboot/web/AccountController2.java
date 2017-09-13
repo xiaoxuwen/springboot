908bd6eea52f6e51d7d6be7c719996178ffa0a0d
= <=>
2017-09-13 17:15:57 +0800

    第七篇：springboot开启声明式事务

---
+++ springboot-mybatis-tx/src/main/java/com/hust/springboot/web/AccountController2.java
@@ -0,0 +1,24 @@
+package com.hust.springboot.web;
+
+import com.hust.springboot.service.AccountService2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fangzhipeng on 2017/4/20.
+ */
+@RestController
+@RequestMapping("/account")
+@MapperScan("com.hust.springboot.dao")
+public class AccountController2 {
+    @Autowired
+    AccountService2 accountService;
+
+    @RequestMapping(value = "/transfer", method = RequestMethod.GET)
+    public void transfer() {
+        accountService.transfer();
+    }
+}
