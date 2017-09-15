257046590927de61a4a06218050e394b733cd0d0
= <=>
2017-09-15 13:09:48 +0800

    第十九篇： 验证表单信息

---
+++ springboot-validate-form/src/main/java/com/hust/springboot/SpringbootValidateFormApplication.java
@@ -0,0 +1,12 @@
+package com.hust.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootValidateFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootValidateFormApplication.class, args);
+    }
+}
